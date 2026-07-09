fe90f91fdd5e7028a78d6fa61e77fcc107d255c0
joris villet <julek.krakow@proton.me>
2026-07-09 21:10:57 +0200

    fix: telegram endpoint; photo from telegram got it and agent can read it. issue with google auth access token

--- src/routes/telegram.ts
+++ src/routes/telegram.ts
@@ -2,114 +2,71 @@ import { Hono } from "hono";
 import type { Env } from "@/types/env";
 import { sendMessage } from "@/utils/telegram/sendMessage";
 import { setAgent } from "@/agent/telegram.llm";
-import * as z from "zod";
-import { zValidator } from "@hono/zod-validator";
+// import * as z from "zod";
+// import { zValidator } from "@hono/zod-validator";
 import { HumanMessage } from "langchain";
+import type { FileInfo } from '@/interfaces/telegram'
+import ky from 'ky'
 
-const schemaTelegram = z.object({
-  update_id: z.number(),
-  message: z.object({
-    message_id: z.number(),
-    from: z.object({
-      id: z.number(),
-      is_bot: z.boolean(),
-      first_name: z.string(),
-      language_code: z.string(),
-    }),
-    chat: z.object({
-      id: z.number(),
-      first_name: z.string(),
-      type: z.string(),
-    }),
-    date: z.number(),
-    text: z.string(),
-    document: z
-      .object({
-        file_name: z.string(),
-        mime_type: z.string(),
-        file_id: z.string(),
-        file_unique_id: z.string(),
-        file_size: z.number(),
-      })
-      .optional(),
-    caption: z.string().optional(),
-    voice: z
-      .object({
-        duration: z.number(),
-        mime_type: z.string(),
-        file_id: z.string(),
-        file_unique_id: z.string(),
-        file_size: z.number(),
-      })
-      .optional(),
-    video_note: z
-      .object({
-        duration: z.number(),
-        length: z.number(),
-        thumbnail: z.any(), // correspond à [Object]
-        thumb: z.any(), // correspond à [Object]
-        file_id: z.string(),
-        file_unique_id: z.string(),
-        file_size: z.number(),
-      })
-      .optional(),
-    photo: z.array(z.any()).optional(), // correspond à Photo[]
-  }),
-});
+// const schemaTelegram = z.object({
+//   update_id: z.number(),
+//   message: z.object({
+//     message_id: z.number(),
+//     from: z.object({
+//       id: z.number(),
+//       is_bot: z.boolean(),
+//       first_name: z.string(),
+//       language_code: z.string(),
+//     }),
+//     chat: z.object({
+//       id: z.number(),
+//       first_name: z.string(),
+//       type: z.string(),
+//     }),
+//     date: z.number(),
+//     text: z.string(),
+//     document: z
+//       .object({
+//         file_name: z.string(),
+//         mime_type: z.string(),
+//         file_id: z.string(),
+//         file_unique_id: z.string(),
+//         file_size: z.number(),
+//       })
+//       .optional(),
+//     caption: z.string().optional(),
+//     voice: z
+//       .object({
+//         duration: z.number(),
+//         mime_type: z.string(),
+//         file_id: z.string(),
+//         file_unique_id: z.string(),
+//         file_size: z.number(),
+//       })
+//       .optional(),
+//     video_note: z
+//       .object({
+//         duration: z.number(),
+//         length: z.number(),
+//         thumbnail: z.any(), // correspond à [Object]
+//         thumb: z.any(), // correspond à [Object]
+//         file_id: z.string(),
+//         file_unique_id: z.string(),
+//         file_size: z.number(),
+//       })
+//       .optional(),
+//     photo: z.array(z.any()).optional(), // correspond à Photo[]
+//   }),
+// });
 
 const app = new Hono<Env>();
 
-// app.post("/message", zValidator("json", schemaTelegram), async (c) => {
-//   try {
-//     console.log("req json", c.req.json());
-//     const body = c.req.valid("json");
-//     const headers = c.req.header();
-//     // console.log("headers => ", headers);
 
-//     const telegramBotApiToken: string | undefined = headers[
-//       "x-telegram-bot-api-secret-token"
-//     ] as string;
-
-//     if (process.env.TELEGRAM_SECRET !== telegramBotApiToken) {
-//       return c.json("No authorized", 400);
-//     }
-
-//     console.log("body telegram => ", body);
-
-//     const userMessage = body.message.text;
-//     const chatId = process.env.TELEGRAM_CHAT_ID;
-//     console.log("user message => ", userMessage);
-
-//     const agent = setAgent();
-//     const humanMessage = new HumanMessage({ content: body.message.text });
-
-//     const responseAgent = (await agent).invoke(
-//       { messages: humanMessage },
-//       { configurable: { thread_id: body.message.chat.id } }
-//     );
-
-//     const aiMessage = (await responseAgent).messages.at(-1)?.content;
-//     console.log("messages length  => ", (await responseAgent).messages.length);
-//     console.log("messages => ", (await responseAgent).messages);
-
-//     if (aiMessage) {
-//       await sendMessage(chatId, aiMessage);
-//     }
-
-//     return c.json("ok", 200);
-//   } catch (err) {
-//     return c.json(err);
-//   }
-// })
-app.get("/message", async (c) => {
-  return c.json("ok", 200);
-})
-
-app.post("/message", zValidator("json", schemaTelegram), async (c) => {
+app.post("/message", async (c) => {
   try {
     // const body = await c.req.json();
-    const body = c.req.valid("json");
-    console.log("body validator => ", body);
+    const body = await c.req.json();
+    console.log("body => ", body);
     const headers = c.req.header();
     // console.log("headers => ", headers);
 
@@ -123,16 +80,91 @@ app.post("/message", zValidator("json", schemaTelegram), async (c) => {
 
     // console.log("body telegram => ", body);
 
-    const userMessage = body.message.text;
+    let message: HumanMessage
+    const tokenTelegram = process.env.TELEGRAM_BOT_TOKEN as string
+    const userMessage = body.message.text || ''
+    const photoTelegram = body.message.photo;
     const chatId = process.env.TELEGRAM_CHAT_ID!;
     console.log("user message => ", userMessage);
 
     const agent = setAgent();
+
+
+    if (photoTelegram) {
+      // console.log("✅ entrée dans photo");
+      // console.log('dernier file_id =>', photoTelegram.at(-1)?.file_id);
+      
+      try {
+
+      const fileInfo = await ky
+        .get(
+          `https://api.telegram.org/bot${tokenTelegram}/getFile?file_id=${photoTelegram.at(-1)?.file_id}`
+        )
+        .json<FileInfo>()
+
+        //console.log("fileInfo =>", fileInfo);
+
+      const filePath = fileInfo.result.file_path
+      const fileBuffer = await ky
+        .get(`https://api.telegram.org/file/bot${tokenTelegram}/${filePath}`)
+        .arrayBuffer()
+
+        //console.log("fileBuffer =>", fileBuffer);
+
+      const base64 = Buffer.from(fileBuffer).toString('base64')
+
+      // console.log("base64 =>", base64);
+
+      message = new HumanMessage({
+        content: [
+          {
+            type: 'text',
+            text:
+              body.message.caption ||
+              'Scan ce ticket de caisse et ajoute le montant TTC et le magasin dans mon tableur de courses',
+          },
+          {
+            type: 'image_url',
+            image_url: {
+              url: `data:image/jpeg;base64,${base64}`,
+            },
+          },
+        ],
+      })
+
+
+
+        const responseAgent = await (await agent).invoke(
+
+            { messages: message },
+
+            { configurable: { thread_id: chatId } }
+
+          );
+
+          const aiMessage = responseAgent.messages.at(-1)?.content;
+
+          console.log("réponse agent photo =>", aiMessage);
+
+          if (aiMessage) await sendMessage(chatId, aiMessage);
+          return;
+
+      } catch (err) {
+
+        console.error("agent photo crashed:", err);
+
+        await sendMessage(chatId, "⚠️ agent crashed sur la photo");
+        return;
+
+      }
+    }
+
+    
     const humanMessage = new HumanMessage({ content: body.message.text });
 
     const responseAgent = (await agent).invoke(
       { messages: humanMessage },
-      { configurable: { thread_id: body.message.chat.id } }
+      { configurable: { thread_id: chatId } }
     );
 
     const aiMessage = (await responseAgent).messages.at(-1)?.content;
